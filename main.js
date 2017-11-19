ee92b311d25a7fc95430da48d2bd21db6042ed99
Courtney Brothers <cournteywbrothers@gmail.com>
2017-11-19 07:38:43 -0600

    css

--- main.js
+++ main.js
@@ -2,50 +2,50 @@ kayakList = [
 	{
   	name:"Mamba",
     price:"$879",
-    description:"our lightest playboat",
+    description:"Our lightest playboat",
     url:"images/kayak1.jpg"
   
   },
   {
   	name :"Zen",
     price:"$579",
-    description:"great for beginners",
+    description:"Great for beginners",
     url:"images/kayak2.jpg"
   },
   {
   	name : "Angler",
     price : "$779",
-    description: "for the seasoned sportsman",
+    description: "For the seasoned sportsman",
     url: "images/kayak3.jpg"
   },
   {
   	name : "Outback",
     price : "$1179",
-    description: "the workhorse",
+    description: "The workhorse",
     url: "images/kayak4.jpg"
   },
   {
   	name : "Barracuda",
     price : "$579",
-    description: "versatile sit-on-top",
+    description: "Versatile sit-on-top",
     url: "images/kayak5.jpg"
   },
   {
   	name : "Kraken",
     price : "$479",
-    description: "speedy and nimble",
+    description: "Speedy and nimble",
     url: "images/kayak6.jpg"
   },
   {
   	name : "Nomad",
     price : "$979",
-    description: "rugged playboat",
+    description: "Rugged playboat",
     url: "images/kayak7.jpg"
   },
   {
   	name : "Big Rig",
     price : "$1279",
-    description: "extra stable and roomy",
+    description: "Extra stable and roomy",
     url: "images/kayak8.jpg"
   }
 
@@ -54,7 +54,7 @@ kayakList = [
 function cardAssign() {
     let empty = "";
 	for (var i=0; i < kayakList.length; i++) {
-        let productName = `<div class="card"><p>name: ${kayakList[i].name}</p><p>price: 	${kayakList[i].price}</p><p>description: ${kayakList[i].description}</p><img src=${kayakList[i].url} alt="kayak product"></div>`;
+        let productName = `<div class="card"><p>Name: ${kayakList[i].name}</p><p>Price: 	${kayakList[i].price}</p><p>Description: ${kayakList[i].description}</p><img src=${kayakList[i].url} alt="kayak product"></div>`;
         empty += productName;
         console.log('this is each card',empty);
     };
